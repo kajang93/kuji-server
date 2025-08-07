a7baf7702e02bc8a31d2750fa22a3a6191b8a708
맥북 <mac@maegbug-ui-MacBookPro.local>
2025-08-07 23:37:50 +0900

    initial commit

---
+++ src/main/java/com/kuji/backend/KujiServerApplication.java
@@ -0,0 +1,13 @@
+package com.kuji.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KujiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KujiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kuji/backend/KujiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuji.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KujiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
